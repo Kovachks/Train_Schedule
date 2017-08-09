7a63fd10824752b91e961e59ae808dde4f57148e
unknown <kovachks90@gmail.comgit config --global kovachks90 Keith>
2017-08-08 23:53:24 -0400

    linked docs

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDE2V6ic5_j6HFgpoQzd2tF_3hngEM8GXI",
+    authDomain: "train-schedule-a2adb.firebaseapp.com",
+    databaseURL: "https://train-schedule-a2adb.firebaseio.com",
+    projectId: "train-schedule-a2adb",
+    storageBucket: "",
+    messagingSenderId: "729415452315"
+  };
+  firebase.initializeApp(config);
